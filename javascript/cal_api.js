f191ce2c7662c87b8a08800351c152dfe33a203b
anthoniraj <anthoniraja@gmail.com>
2024-02-27 07:21:11 +0530

    added js api script

--- javascript/cal_api.js
+++ javascript/cal_api.js
@@ -5,13 +5,13 @@ function getResponse(response){
     return response.json(); // Parse the response body as JSON
 }
 function displayData(data){
-    console.log(data);
+    console.log(data['meanings']);
 }
 function displayError(error){
     console.error('There was a problem with the fetch operation:', error);
 }
  
-fetch('http://10.30.33.23:5000/meaning/hello')
+fetch('http://127.0.0.1:5000/meaning/fetch')
   .then(response => getResponse(response))  
   .then(data => displayData(data))
-  .catch(error => displayError(error));
+  .catch(error => displayError(error.message));
