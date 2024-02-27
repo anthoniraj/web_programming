27280679ef3f8570f30afffe3b0352076484f1f6
anthoniraj <anthoniraja@gmail.com>
2024-02-27 07:38:02 +0530

    added fetch function script

--- javascript/cal_api.js
+++ javascript/cal_api.js
@@ -1,17 +1,28 @@
-function getResponse(response){
+function getResponse(response) {
     if (!response.ok) {
         throw new Error('Network response was not ok');
     }
     return response.json(); // Parse the response body as JSON
 }
-function displayData(data){
+function displayData(data) {
     console.log(data['meanings']);
 }
-function displayError(error){
+function displayError(error) {
     console.error('There was a problem with the fetch operation:', error);
 }
- 
+
 fetch('http://127.0.0.1:5000/meaning/fetch')
-  .then(response => getResponse(response))  
-  .then(data => displayData(data))
-  .catch(error => displayError(error.message));
+    .then(response => getResponse(response))
+    .then(data => displayData(data))
+    .catch(error => displayError(error.message));
+
+/* Simplefied fecth function */
+fetch('http://127.0.0.1:5000/meaning/fetch')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json(); // Parse the response body as JSON
+    })
+    .then(data => console.log(data['meanings']))
+    .catch(error => console.error('There was a problem with the fetch operation:', error));
