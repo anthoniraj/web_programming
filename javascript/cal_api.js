549c7f273f40a5d6eba7ed8aaf56b717a978796d
anthoniraj <anthoniraja@gmail.com>
2024-02-27 07:40:52 +0530

    added fetch script output

--- javascript/cal_api.js
+++ javascript/cal_api.js
@@ -26,3 +26,19 @@ fetch('http://127.0.0.1:5000/meaning/fetch')
     })
     .then(data => console.log(data['meanings']))
     .catch(error => console.error('There was a problem with the fetch operation:', error));
+
+
+/* Output 
+[
+    'the action of fetching',
+    'go or come after and bring or take back',
+    'be sold for a certain price',
+    'take away or remove'
+    ]
+    [
+    'the action of fetching',
+    'go or come after and bring or take back',
+    'be sold for a certain price',
+    'take away or remove'
+    ]
+*/
