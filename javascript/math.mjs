1b9e12148e4e6f7561780831c51acf96eebfeb8e
anthoniraj <anthoniraja@gmail.com>
2024-03-11 19:35:31 +0530

    added error handling js code

--- javascript/math.mjs
+++ javascript/math.mjs
@@ -6,7 +6,7 @@ function caps(str){
     try{
         if(str === null){
             throw new Error('String Expected. NULL is GIVEN');
-        }else if(/*check if it is a number*/){
+        }else if(typeof(str) == 'number'){
             throw new Error('String Expected. Number is GIVEN');
         }
         return str.toUpperCase();
